2b134e08a79459143cbdaab452938977066549f4
ToughGuyClub(laptab) <gyudong6185@gmail.com>
2025-09-22 12:42:50 +0900

    add walk

--- animation_viewer.py
+++ animation_viewer.py
@@ -7,10 +7,24 @@ Quagsire_idle=load_image('Idle-Anim.png')
 Quagsire_shoot=load_image('Shoot-Anim.png')
 Quagsire_sleep=load_image('Sleep-Anim.png')
 
+
+def animation_walk():
+    for n in range(0,500):
+        frame = 0
+        for x in range(4):
+            clear_canvas()
+            Quagsire_walk.clip_draw(frame*48,200,48,40,400,200,300,300)
+            frame=(frame+1)%4
+            update_canvas()
+            delay(0.1)
+
+
 # 여기를 채우세요.
-for x in range(0,800):
+while(True):
     clear_canvas()
-    Quagsire_shoot.draw(400, 30)
+    #그림 및 애니메이션
+    animation_walk()
+
     update_canvas()
     delay(0.01)
 
